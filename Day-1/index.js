462bb06c91dfa1c9e18cf1f574d43211876dd476
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-20 01:03:31 +0530

    docker

--- Day-1/index.js
+++ Day-1/index.js
@@ -262,4 +262,74 @@ Status: Downloaded newer image for ubuntu:latest
 vishal sanam@LAPTOP-GHS5GD2E MINGW64 ~/Downloads/Docker/Day-1 (main)
 $ docker run ubuntu 
 
+
+
+vishal sanam@LAPTOP-GHS5GD2E MINGW64 ~/Downloads/Docker/Day-1 (main)
+$ docker run -d nginx
+24d433d8ab54fdf1ad327fce757ca6359bc5c6ad2f8406bde073d85dca72eba5
+
+vishal sanam@LAPTOP-GHS5GD2E MINGW64 ~/Downloads/Docker/Day-1 (main)
+$ docker run --name my-nignx ngnix
+Unable to find image 'ngnix:latest' locally
+docker: Error response from daemon: pull access denied for ngnix, repository does not exist or may require 'docker login'
+
+Run 'docker run --help' for more information
+
+vishal sanam@LAPTOP-GHS5GD2E MINGW64 ~/Downloads/Docker/Day-1 (main)
+$ docker run --name my-ngnix ngnix
+Unable to find image 'ngnix:latest' locally
+docker: Error response from daemon: pull access denied for ngnix, repository does not exist or may require 'docker login'
+
+Run 'docker run --help' for more information
+
+vishal sanam@LAPTOP-GHS5GD2E MINGW64 ~/Downloads/Docker/Day-1 (main)
+$ docker run --name my-ngnix ngnix
+Unable to find image 'ngnix:latest' locally
+docker: Error response from daemon: pull access denied for ngnix, repository does not exist or may require 'docker login'
+
+Run 'docker run --help' for more information
+
+vishal sanam@LAPTOP-GHS5GD2E MINGW64 ~/Downloads/Docker/Day-1 (main)
+$ docker run --name my-nginx nginx
+/docker-entrypoint.sh: /docker-entrypoint.d/ is not empty, will attempt to perform configuration
+/docker-entrypoint.sh: Looking for shell scripts in /docker-entrypoint.d/
+/docker-entrypoint.sh: Launching /docker-entrypoint.d/10-listen-on-ipv6-by-default.sh
+10-listen-on-ipv6-by-default.sh: info: Getting the checksum of /etc/nginx/conf.d/default.conf
+10-listen-on-ipv6-by-default.sh: info: Enabled listen on IPv6 in /etc/nginx/conf.d/default.conf
+/docker-entrypoint.sh: Sourcing /docker-entrypoint.d/15-local-resolvers.envsh
+/docker-entrypoint.sh: Launching /docker-entrypoint.d/20-envsubst-on-templates.sh
+/docker-entrypoint.sh: Launching /docker-entrypoint.d/30-tune-worker-processes.sh
+/docker-entrypoint.sh: Configuration complete; ready for start up
+2025/09/19 19:31:21 [notice] 1#1: using the "epoll" event method
+2025/09/19 19:31:21 [notice] 1#1: nginx/1.29.1
+2025/09/19 19:31:21 [notice] 1#1: built by gcc 12.2.0 (Debian 12.2.0-14+deb12u1)
+2025/09/19 19:31:21 [notice] 1#1: OS: Linux 6.6.87.1-microsoft-standard-WSL2
+2025/09/19 19:31:21 [notice] 1#1: getrlimit(RLIMIT_NOFILE): 1048576:1048576
+2025/09/19 19:31:21 [notice] 1#1: start worker processes
+2025/09/19 19:31:21 [notice] 1#1: start worker process 29
+2025/09/19 19:31:21 [notice] 1#1: start worker process 30
+2025/09/19 19:31:21 [notice] 1#1: start worker process 31
+2025/09/19 19:31:21 [notice] 1#1: start worker process 32
+2025/09/19 19:31:21 [notice] 1#1: start worker process 33
+2025/09/19 19:31:21 [notice] 1#1: start worker process 34
+2025/09/19 19:31:21 [notice] 1#1: start worker process 35
+2025/09/19 19:31:21 [notice] 1#1: start worker process 36
+2025/09/19 19:31:21 [notice] 1#1: start worker process 37
+2025/09/19 19:31:21 [notice] 1#1: start worker process 38
+2025/09/19 19:31:21 [notice] 1#1: start worker process 39
+2025/09/19 19:31:21 [notice] 1#1: start worker process 40
+
+vishal sanam@LAPTOP-GHS5GD2E MINGW64 ~/Downloads/Docker/Day-1 (main)
+$ docker run --name my-nginx -d nginx
+ecc5b55cbdf298c3fd3c0f92b5acc9112b4cf135a1c3097d6ae42b43a855cfe4
+
+vishal sanam@LAPTOP-GHS5GD2E MINGW64 ~/Downloads/Docker/Day-1 (main)
+$ docker ps
+CONTAINER ID   IMAGE     COMMAND                  CREATED          STATUS          PORTS     NAMES
+ecc5b55cbdf2   nginx     "/docker-entrypoint.…"   25 seconds ago   Up 24 seconds   80/tcp    my-nginx
+24d433d8ab54   nginx     "/docker-entrypoint.…"   7 minutes ago    Up 7 minutes    80/tcp    musing_rosalind
+
+vishal sanam@LAPTOP-GHS5GD2E MINGW64 ~/Downloads/Docker/Day-1 (main)
+$
+
 */
