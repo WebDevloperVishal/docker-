516a486f240715fa9ea9565a8cb011b27439c974
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-07 07:37:07 +0530

    docker

--- Day-1/Data.js
+++ Day-1/Data.js
@@ -1,8 +1,12 @@
 /*
 $ docker images
+$ docker images ls
 $ docker pull ubuntu:20.04
 $ docker pull mongodb/mongodb-community-server:latest 
 $ docker rmi ubuntu:20.04
 $ docker pull ubuntu:20.04
 $ docker image prune -a 
+$ docker pull nginx
+$ docker run nginx
+$ docker run ubuntu
 */
